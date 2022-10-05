fc9f230b5ee86c4e8c5b47a64bea37b4cfabdea7
KimBeomJin <sdpthf@naver.com>
2022-10-05 22:10:14 +0900

    chore: springdoc dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
