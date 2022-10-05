94195cf5824d626e43be3d567aacbb5e5019350d
KimBeomJin <sdpthf@naver.com>
2022-10-05 20:06:28 +0900

    add: SecurityConfig

---
+++ src/main/kotlin/meister/hackaton/maskserver/global/security/SecurityConfig.kt
@@ -0,0 +1,36 @@
+package meister.hackaton.maskserver.global.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    protected fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors().and()
+            .csrf().disable()
+            .formLogin().disable()
+
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        http
+            .authorizeRequests()
+
+            .anyRequest().authenticated()
+
+        return http.build()
+    }
+
+    @Bean
+    protected fun PasswordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+}
