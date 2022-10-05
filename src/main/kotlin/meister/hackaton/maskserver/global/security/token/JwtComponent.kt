7ca98de4c0efe11d2f00802c3b5c3015c5dd683f
KimBeomJin <sdpthf@naver.com>
2022-10-05 20:07:17 +0900

    add: JwtComponent

---
+++ src/main/kotlin/meister/hackaton/maskserver/global/security/token/JwtComponent.kt
@@ -0,0 +1,7 @@
+package meister.hackaton.maskserver.global.security.token
+
+object JwtComponent {
+    const val PREFIX = "Bearer "
+    const val HEADER = "Authorization"
+    const val AUTHORITY = "authority"
+}
