e95342ebba768063afdac089197b2b63ca735740
KimBeomJin <sdpthf@naver.com>
2022-10-06 00:21:18 +0900

    chore: aws dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
 
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
 
+	implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+
 	runtimeOnly("mysql:mysql-connector-java")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
