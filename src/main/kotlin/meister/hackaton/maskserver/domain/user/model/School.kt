0c0205cd2531160141da426c24eb54e81773ca12
KimBeomJin <sdpthf@naver.com>
2022-10-05 20:06:45 +0900

    add: School

---
+++ src/main/kotlin/meister/hackaton/maskserver/domain/user/model/School.kt
@@ -0,0 +1,12 @@
+package meister.hackaton.maskserver.domain.user.model
+
+enum class School(
+    val school: String
+) {
+
+    DSM("대덕소프트웨어마이스터고등학교"),
+    DGSW("대구소프트웨어마이스터고등학교"),
+    GSM("광주소프트웨어마이스터고등학교"),
+    BSSM("부산소프트웨어마이스터고등학교")
+
+}
