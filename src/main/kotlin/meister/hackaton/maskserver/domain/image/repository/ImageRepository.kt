bf97a11692472ce870e739668790841824ccde81
KimBeomJin <sdpthf@naver.com>
2022-10-06 01:46:10 +0900

    remove: Image

--- src/main/kotlin/meister/hackaton/maskserver/domain/image/repository/ImageRepository.kt
+++
@@ -1,9 +0,0 @@
-package meister.hackaton.maskserver.domain.image.repository
-
-import meister.hackaton.maskserver.domain.image.model.Image
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ImageRepository : CrudRepository<Image, Long> {
-}
