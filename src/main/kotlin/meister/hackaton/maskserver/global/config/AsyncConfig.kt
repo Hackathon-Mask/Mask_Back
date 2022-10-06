ac166aa858d2c2d8277404c09f050f5ec5c174ea
KimBeomJin <sdpthf@naver.com>
2022-10-06 22:47:20 +0900

    chore: 비동기 이벤트 처리

---
+++ src/main/kotlin/meister/hackaton/maskserver/global/config/AsyncConfig.kt
@@ -0,0 +1,9 @@
+package meister.hackaton.maskserver.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@EnableAsync
+@Configuration
+class AsyncConfig {
+}
--- src/main/kotlin/meister/hackaton/maskserver/global/event/CoffeeChatArrivalEventHandler.kt
+++ src/main/kotlin/meister/hackaton/maskserver/global/event/CoffeeChatArrivalEventHandler.kt
@@ -2,6 +2,7 @@ package meister.hackaton.maskserver.global.event
 
 import meister.hackaton.maskserver.domain.coffeechat.event.CoffeeChatArrivalEvent
 import meister.hackaton.maskserver.thirdparty.message.MessageSender
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
@@ -11,6 +12,7 @@ class CoffeeChatArrivalEventHandler(
     private val messageSender: MessageSender
 ) {
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun sendMessage(event: CoffeeChatArrivalEvent) {
         println("CoffeeChatArrivalEvent activated")
