43e458a296048271e57a452453a1b9495559e9ea
KimBeomJin <sdpthf@naver.com>
2022-10-06 10:12:48 +0900

    chore: create question permission

--- src/main/kotlin/meister/hackaton/maskserver/global/security/SecurityConfig.kt
+++ src/main/kotlin/meister/hackaton/maskserver/global/security/SecurityConfig.kt
@@ -48,9 +48,6 @@ class SecurityConfig(
             .antMatchers(HttpMethod.GET, "/tags/majors").permitAll()
             .antMatchers(HttpMethod.GET, "/tags/skills").permitAll()
 
-            // questions
-            .antMatchers(HttpMethod.POST, "/questions").permitAll()
-
             .anyRequest().authenticated()
 
         http
